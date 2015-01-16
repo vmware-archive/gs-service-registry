c586e6a69f69686d851833af128faeaa1efd8094
Craig Walls <cwalls@pivotal.io>
2015-01-16 15:23:16 -0600

    Add initial and complete service project

---
+++ complete/greeting-service/src/main/java/hello/DemoApplication.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ initial/greeting-service/src/main/java/hello/DemoApplication.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
