a0cd9d19186990677427cf5a545eb37e1c1e0019
Craig Walls <cwalls@pivotal.io>
2015-01-19 15:53:33 -0600

    Add consumption section to guide

---
+++ complete/greeting-client/src/main/java/hello/Greeting.java
@@ -0,0 +1,25 @@
+package hello;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Greeting {
+
+    private long id;
+    private String content;
+
+    @JsonCreator
+    public Greeting(@JsonProperty("id") long id, @JsonProperty("content") String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
---
+++ complete/greeting-client/src/main/java/hello/GreetingClientController.java
@@ -0,0 +1,26 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/hello")
+public class GreetingClientController {
+
+	private RestTemplate rest;
+
+    @Autowired
+	public GreetingClientController(RestTemplate rest) {
+		this.rest = rest;
+	}
+
+        @RequestMapping(method=RequestMethod.GET)
+        public String hello() {
+			Greeting greeting = rest.getForObject("http://GreetingService/greeting", Greeting.class);
+        	return greeting.getContent();
+        }
+        
+}
