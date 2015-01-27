f4d7f9bedb470169e210e60341a13d8a42438415
Roy Clarkson <rclarkson@pivotal.io>
2015-01-26 21:27:46 -0600

    Remove @EnableDiscoveryClient in 'initial' code

--- initial/greeting-client/src/main/java/hello/DemoApplication.java
+++ initial/greeting-client/src/main/java/hello/DemoApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@EnableDiscoveryClient
 public class DemoApplication {
 
     public static void main(String[] args) {
